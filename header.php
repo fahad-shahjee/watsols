3d7038628bae9cbff7c6ebb2792b5463603a10b6
fahad-shahjee <cyberfahad021@gmail.com>
2025-09-25 17:24:59 +0500

    initial commit

---
+++ header.php
@@ -0,0 +1,74 @@
+<div class="navbar-wrapper">
+    <nav class="navbar navbar-expand-xl">
+        <div class="navbar-container">
+            <div class="logo-container">
+                <a class="navbar-brand" href="./index.php"><img src="./image/marko-logo.png" class="site-logo img-fluid"></a>
+            </div>
+            <button class="navbar-toggler nav-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars "></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Services <i class="fa-solid fa-angle-down accent-color"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="service.php">Service</a></li>
+                            <li><a class="dropdown-item" href="single_services.php">Single Services</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Pages <i class="fa-solid fa-angle-down accent-color"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="case_studies.php">Case Studies</a></li>
+                            <li><a class="dropdown-item" href="team.php">Our Team</a></li>
+                            <li><a class="dropdown-item" href="partnership.php">Partnership</a></li>
+                            <li><a class="dropdown-item" href="pricing.php">Princing Plan</a></li>
+                            <li><a class="dropdown-item" href="testimonial.php">Testimonial</a></li>
+                            <li><a class="dropdown-item" href="faq.php">FAQs</a></li>
+                            <li><a class="dropdown-item" href="404_page.php">Error 404</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Archive <i class="fa-solid fa-angle-down accent-color"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="blog.php">Blog</a></li>
+                            <li><a class="dropdown-item" href="single_post.php">Single Post</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="navbar-action-container">
+                <div class="navbar-action-button">
+                    <button id="themeSwitch" class="themeswitch" aria-label="Toggle Theme">
+                        <i id="themeIcon" class="fas fa-moon"></i>
+                    </button>                      
+                </div>
+                <div class="navbar-icon-wrapper">
+                    <div class="icon-circle">
+                        <i class="fa-solid fa-phone-volume"></i>
+                    </div>
+                    <h6>+1 (62) 987 7543</h6>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
