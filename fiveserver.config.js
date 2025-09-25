860fa271352b47ac4f47f5ec48b4d9c2cc7ea817
fahad-shahjee <cyberfahad021@gmail.com>
2025-09-25 18:03:29 +0500

    starting work

---
+++ fiveserver.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  php: "D:\\laragon\\bin\\php\\php-8.4.8\\php.exe"
+}
--- header.php
+++ header.php
@@ -2,65 +2,54 @@
     <nav class="navbar navbar-expand-xl">
         <div class="navbar-container">
             <div class="logo-container">
-                <a class="navbar-brand" href="./index.php"><img src="./image/marko-logo.png" class="site-logo img-fluid"></a>
+                <a class="navbar-brand" href="./index.php">
+                    <img src="./image/marko-logo.png" class="site-logo img-fluid" alt="Logo">
+                </a>
             </div>
             <button class="navbar-toggler nav-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa-solid fa-bars "></i>
+                <i class="fa-solid fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mx-auto">
+                    <!-- Home -->
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="index.php">Home</a>
                     </li>
+
+                    <!-- About -->
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="about.php">About</a>
+                        <a class="nav-link" href="about.php">About</a>
                     </li>
+
+                    <!-- Services with 5 Submenus -->
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                             aria-expanded="false">
                             Services <i class="fa-solid fa-angle-down accent-color"></i>
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="service.php">Service</a></li>
-                            <li><a class="dropdown-item" href="single_services.php">Single Services</a></li>
+                            <li><a class="dropdown-item" href="service1.php">Service 1</a></li>
+                            <li><a class="dropdown-item" href="service2.php">Service 2</a></li>
+                            <li><a class="dropdown-item" href="service3.php">Service 3</a></li>
+                            <li><a class="dropdown-item" href="service4.php">Service 4</a></li>
+                            <li><a class="dropdown-item" href="service5.php">Service 5</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Pages <i class="fa-solid fa-angle-down accent-color"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="case_studies.php">Case Studies</a></li>
-                            <li><a class="dropdown-item" href="team.php">Our Team</a></li>
-                            <li><a class="dropdown-item" href="partnership.php">Partnership</a></li>
-                            <li><a class="dropdown-item" href="pricing.php">Princing Plan</a></li>
-                            <li><a class="dropdown-item" href="testimonial.php">Testimonial</a></li>
-                            <li><a class="dropdown-item" href="faq.php">FAQs</a></li>
-                            <li><a class="dropdown-item" href="404_page.php">Error 404</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Archive <i class="fa-solid fa-angle-down accent-color"></i>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="blog.php">Blog</a></li>
-                            <li><a class="dropdown-item" href="single_post.php">Single Post</a></li>
-                        </ul>
-                    </li>
+
+                    <!-- Contact Us -->
                     <li class="nav-item">
                         <a class="nav-link" href="contact.php">Contact Us</a>
                     </li>
                 </ul>
             </div>
+
+            <!-- Right-side action buttons (unchanged) -->
             <div class="navbar-action-container">
                 <div class="navbar-action-button">
                     <button id="themeSwitch" class="themeswitch" aria-label="Toggle Theme">
                         <i id="themeIcon" class="fas fa-moon"></i>
-                    </button>                      
+                    </button>
                 </div>
                 <div class="navbar-icon-wrapper">
                     <div class="icon-circle">
@@ -72,3 +61,4 @@
         </div>
     </nav>
 </div>
+<!-- End Navbar -->
