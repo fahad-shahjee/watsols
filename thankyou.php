bb626caab0c2b92ca3cbac8e344cf4a8c2f3812a
fahad-shahjee <cyberfahad021@gmail.com>
2025-10-02 22:31:50 +0500

    Backend Done

--- thankyou.php
+++ thankyou.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Error 404 - Watsols</title>
+    <title>Thank You - Watsols</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="icon" href="assets/image/favicon.ico">
 </head>
@@ -23,8 +23,8 @@
         <div class="section-404">
             <div class="banner-layout-404">
                 <div class="layout-404">
-                    <span class="text-404 title-heading animate-box animated animate__animated" data-animate="animate__fadeInRight">404</span>
-                    <h3>Thank You!</h3>
+                    <span class="text-404 title-heading animate-box animated animate__animated" data-animate="animate__fadeInRight">Thankyou</span>
+                    <h3>Your message has been received successfully</h3>
                     <p>We appreciate you reaching out. Our team will get back to you as soon as possible.</p>
                     <div>
                         <a href="./index.php" class="btn btn-accent">
